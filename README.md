2384c3c9e0f0837dfc476d154b6de92eec48d1fd
ufoo68 <mitarou.yuutarou@gmail.com>
2020-05-03 09:49:56 +0900

    Update README

--- README.md
+++ README.md
@@ -1,14 +1,9 @@
-# Welcome to your CDK TypeScript project!
+# オンライン施設案内用API
 
-This is a blank project for TypeScript development with CDK.
+[おうちハッカソン](https://connpass.com/event/174573/)にて作成。以下のオンライン施設（アミューズメント）の情報を取得できるAPIです。
 
-The `cdk.json` file tells the CDK Toolkit how to execute your app.
+- オンライン水族館
+- オンライン神社
+- オンライン温泉
 
-## Useful commands
-
- * `npm run build`   compile typescript to js
- * `npm run watch`   watch for changes and compile
- * `npm run test`    perform the jest unit tests
- * `cdk deploy`      deploy this stack to your default AWS account/region
- * `cdk diff`        compare deployed stack with current state
- * `cdk synth`       emits the synthesized CloudFormation template
+このAPIは[GraphQL](https://graphql.org/)の形で提供しております。また実際にこのAPIを用いたオンライン施設情報の投稿用フォームを[こちら](https://github.com/ufoo68/amusement-post-form)で提供しております。
--- lib/online-amusement-api-stack.ts
+++ lib/online-amusement-api-stack.ts
@@ -1,9 +1,90 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from '@aws-cdk/core'
+import { GraphQLApi, CfnApiKey, MappingTemplate, Values, PartitionKey, KeyCondition } from '@aws-cdk/aws-appsync'
+import { Table, AttributeType } from '@aws-cdk/aws-dynamodb'
 
 export class OnlineAmusementApiStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    super(scope, id, props)
 
-    // The code that defines your stack goes here
+    const api = new GraphQLApi(this, 'graphQlApi', {
+      name: 'online-amusement-api',
+      schemaDefinitionFile: './schema.graphql',
+    })
+
+    new CfnApiKey(this, 'apiKey', {
+      apiId: api.apiId,
+    })
+
+    const aquariumTable = new Table(this, 'aquariumTable', {
+      partitionKey: {
+        name: 'id',
+        type: AttributeType.STRING,
+      },
+    })
+
+    const shrimeTable = new Table(this, 'shrimeTable', {
+      partitionKey: {
+        name: 'id',
+        type: AttributeType.STRING,
+      },
+    })
+
+    const onsenTable = new Table(this, 'onsenTable', {
+      partitionKey: {
+        name: 'id',
+        type: AttributeType.STRING,
+      },
+    })
+
+    const aquariumDS = api.addDynamoDbDataSource('aquarium', 'aquarium data source', aquariumTable)
+    aquariumDS.createResolver({
+      typeName: 'Query',
+      fieldName: 'getAquarium',
+      requestMappingTemplate: MappingTemplate.dynamoDbScanTable(),
+      responseMappingTemplate: MappingTemplate.dynamoDbResultList(),
+    })
+    aquariumDS.createResolver({
+      typeName: 'Mutation',
+      fieldName: 'addAquarium',
+      requestMappingTemplate: MappingTemplate.dynamoDbPutItem(
+        PartitionKey.partition('id').auto(),
+        Values.projecting('amusement'),
+      ),
+      responseMappingTemplate: MappingTemplate.dynamoDbResultItem(),
+    })
+
+    const shrimeDS = api.addDynamoDbDataSource('shrime', 'shrime data source', shrimeTable)
+    shrimeDS.createResolver({
+      typeName: 'Query',
+      fieldName: 'getShrime',
+      requestMappingTemplate: MappingTemplate.dynamoDbScanTable(),
+      responseMappingTemplate: MappingTemplate.dynamoDbResultList(),
+    })
+    shrimeDS.createResolver({
+      typeName: 'Mutation',
+      fieldName: 'addShrime',
+      requestMappingTemplate: MappingTemplate.dynamoDbPutItem(
+        PartitionKey.partition('id').auto(),
+        Values.projecting('amusement'),
+      ),
+      responseMappingTemplate: MappingTemplate.dynamoDbResultItem(),
+    })
+
+    const onsenDS = api.addDynamoDbDataSource('onsen', 'onsen data source', onsenTable)
+    onsenDS.createResolver({
+      typeName: 'Query',
+      fieldName: 'getOnsen',
+      requestMappingTemplate: MappingTemplate.dynamoDbScanTable(),
+      responseMappingTemplate: MappingTemplate.dynamoDbResultList(),
+    })
+    onsenDS.createResolver({
+      typeName: 'Mutation',
+      fieldName: 'addOnsen',
+      requestMappingTemplate: MappingTemplate.dynamoDbPutItem(
+        PartitionKey.partition('id').auto(),
+        Values.projecting('amusement'),
+      ),
+      responseMappingTemplate: MappingTemplate.dynamoDbResultItem(),
+    })
   }
 }
